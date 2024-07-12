567832df349b39444756fee4b259067b9643577f
jianghua1 <719584530@qq.com>
2024-07-12 17:29:16 +0800

    sec commit

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -26,15 +26,6 @@
     <el-button type="warning" round>Warning</el-button>
     <el-button type="danger" round>Danger</el-button>
   </div>
-
-  <div>
-    <el-button :icon="Search" circle />
-    <el-button type="primary" :icon="Edit" circle />
-    <el-button type="success" :icon="Check" circle />
-    <el-button type="info" :icon="Message" circle />
-    <el-button type="warning" :icon="Star" circle />
-    <el-button type="danger" :icon="Delete" circle />
-  </div>
 </template>
 
 <script setup lang="ts">
